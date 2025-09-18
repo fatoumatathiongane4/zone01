395f59ca0224be540bfd744021486d053539f11a
fatoumatathiongane4 <fatoumata.thiongane4@gmail.com>
2025-09-18 23:47:23 +0000

    Add files via upload

---
+++ fatima.py
@@ -0,0 +1,27 @@
+prenom = "fatima"
+age = 18
+taille = 1.70
+
+print(prenom)
+print(age)
+print(taille)
+
+if taille <1.60:
+    print("tu es petite")
+
+elif taille < 1.90 :
+    print("tu as une taille moyenne ")
+
+else:
+    print( "tu nest pas petite")  
+
+for i in range (age):
+    print(i) 
+    
+print("111111111111111111111111111111111111111111111111111111111111111")
+def multiplie3nombre(a,b,c):
+    result = a*b*c
+    print (result) 
+
+
+multiplie3nombre(1,2,3)
